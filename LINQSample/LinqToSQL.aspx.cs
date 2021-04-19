c59b94b62ac4615274d184faaf797dac81fa9b57
melon1313 <10212130@gm.nttu.edu.tw>
2021-04-19 21:12:51 +0800

    加入專案檔案。

---
+++ LINQSample/LinqToSQL.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LINQSample
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SampleDataContext dataContext = new SampleDataContext();
+            GridView1.DataSource = from gamer in dataContext.Gamer
+                                   where gamer.Gender == "Female"
+                                   select gamer;
+            
+            //GridView1.DataSource = dataContext.Gamers.Where(gamer => gamer.Gender == "Female");
+            GridView1.DataBind();
+        }
+    }
+
+    public class GamerA
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+}
