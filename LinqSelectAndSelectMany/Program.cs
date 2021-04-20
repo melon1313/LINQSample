7564dbd06dd46ea28dfbf16d137569e4d491276e
melon1313 <10212130@gm.nttu.edu.tw>
2021-04-20 22:36:43 +0800

    抽出共用Data

--- LinqSelectAndSelectMany/Program.cs
+++ LinqSelectAndSelectMany/Program.cs
@@ -48,31 +48,4 @@ namespace LinqSelectAndSelectMany
             Console.ReadLine();
         }
     }
-
-    public class Gamer
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Gender { get; set; }
-        public int Score { get; set; }
-        public List<string> Skills { get; set; }
-    }
-
-    public class GamerHelper
-    {
-        public static List<Gamer> GetSampleGamers()
-        {
-            return new List<Gamer>
-            {
-                new Gamer{Id=1,Name="Name01",Gender="Male", Score =6000,
-                    Skills = new List<string>{"SkilA", "SkillB", "SkillC"}},
-                new Gamer{Id=2,Name="Name02",Gender="Male", Score =3000,
-                    Skills = new List<string>{"SkilA", "SkillD"}},
-                new Gamer{Id=3,Name="Name03",Gender="Female", Score =4500,
-                    Skills = new List<string>{"SkilC", "SkillE"}},
-                new Gamer{Id=4,Name="Name04",Gender="Male", Score =8000,
-                    Skills = new List<string>{"SkilA", "SkillB", "SkillC", "SkillD"}},
-            };
-        }
-    }
 }
